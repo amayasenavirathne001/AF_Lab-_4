7413b467d57ebe28409363485c84117f38c3f9cb
Amaya <amayasenavirathne001@gmail.com>
2026-02-23 16:20:11 +0530

    Implemented  HTTP GET request using https module

--- app.js
+++ app.js
@@ -12,10 +12,27 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   console.log('File saved!');
 });*/
 
-const http = require('http');
+/*const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!');
   res.end();
-}).listen(8080);
+}).listen(8080);*/
+
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
